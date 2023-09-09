8f19016c14613e313552cc504025de6e44a4c1f7
Andreas Noskinnen <130252943+noskinnen@users.noreply.github.com>
2023-09-09 21:12:29 +0300

    update web pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,42 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin"), // Here!
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './js/index.js',
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
     module: {
         rules: [
-          { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: "asset/resource",
-          },
-        ]
-      },
-      output: {
+            {
+                test: /.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-      },
-      plugins: [
-        // And here!
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [
         new CopyPlugin({
-          patterns: [
-            { from: "static", to: "static" }
-          ],
+            patterns: [{ from: 'static', to: 'static' }],
         }),
         new HtmlWebpackPlugin({
-          template: "./dist/bundle.js"
+            template: './index.html',
         }),
-      ]
-    }
+        new MiniCssExtractPlugin(),
+    ],
+};
