2edb7cc1d977449ba2596c1d107b968206308ed3
Andreas Noskinnen <130252943+noskinnen@users.noreply.github.com>
2023-09-20 16:43:04 +0300

    update 4.5

--- webpack.config.js
+++ webpack.config.js
@@ -9,21 +9,26 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.css$/,
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
-                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
             {
-                test: /.(woff|woff2|eot|ttf|otf)$/i,
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
         ],
     },
     resolve: {
-        extensions: [".ts", ".js"],
+        extensions: ['.ts', '.js'],
     },
     optimization: {
         minimizer: ['...', new CssMinimizerPlugin()],
